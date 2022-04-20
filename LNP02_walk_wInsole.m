97c247f669974e6663856b4826d4e1fa9dc00095
dps <dpsarmaji@gmail.com>
2022-04-20 18:24:12 -0400

    update

--- LNP02_walk_wInsole.m
+++ LNP02_walk_wInsole.m
@@ -6,6 +6,8 @@
 disp('Please Select the Ripple Data Folder');
 [tmpFilenames, emgPathname] = uigetfile(['C:/data/LL_UH3/' '*.pkl'],'Pick files','MultiSelect', 'on');
 nevPath = '\\share.files.pitt.edu\RnelShare\data_raw\human\uh3_stim\LNP02\data\Closed Loop\';
+insolePath = '\\share.files.pitt.edu\RnelShare\data_raw\human\uh3_stim\LNP02\data\Closed Loop\Insole_data';
+
 % Make File List
 for f = 1:length(tmpFilenames)
     emgFilenames{f} = tmpFilenames(f);
@@ -20,10 +22,11 @@ mLabels = {"Right TFL", "Right RF", "Right TA", "Right SO", "Right LG", "Right V
 chan_remap = [1 2 6 10 14 3 4 5 7 8 9 11 12 13 15 16]; %To match actual Delsys order
 
 for f = 1:length(emgFilenames)
-    %% Find File
-    clear fstruct emg filename data
+    %% Find Files
+    clear fstruct emg filename data tmpf insole_data
     filename = trialFilenames{f};
     fstruct = dir([nevPath cell2mat(filename) '*.nev']);
+    istruct  = dir([insolePath cell2mat(filename) '*.json']);
     
     disp('loading EMG');
     %% Get EMG Data
@@ -44,7 +47,38 @@ for f = 1:length(emgFilenames)
     emg = filtfilt(b,a,data.emg')';
     
     tend = length(data.time)/fs;
+
+    %% Get Insole Data
     
+    tmpf = py.open(istruct); % Opening JSON file
+    insole_data = py.json.load(tmpf);
+    py.scipy.io.savemat('C:/data/tmp_insole.mat', mdict=struct('insole_data', insole_data));
+    load('C:\data\tmp_insole.mat');
+    for i = 1:length(data)
+        t_left(i) = data{i}.time_L;
+        left(:,i) = data{i}.pressure_L;
+        cop_L(i) = data{i}.cop_L;
+        t_right(i) = data{i}.time_R;
+        right(:,i) = data{i}.pressure_R;
+        cop_R(i) = data{i}.cop_R;
+    end
+
+    t_left = t_left - t_left(1);
+    t_right = t_right - t_right(1);
+    %Find phases Left
+    mask = logical(cop_L(:).');    %(:).' to force row vector
+    stops_L = strfind([false, mask], [0 1]);
+    starts_L = strfind([mask, false], [1 0]);
+    centers_L = mean([starts_L;stops_L]);
+    
+    %Find phases Right
+    mask = logical(cop_R(:).');    %(:).' to force row vector
+    stops_R = strfind([false, mask], [0 1]);
+    starts_R = strfind([mask, false], [1 0]);
+    centers_R = mean([starts_R;stops_R]);
+
+    
+    %% Get Stims
     if isempty(fstruct)
         disp([filename ' does not have .nev']);
         stimStatus = 2;
@@ -70,26 +104,54 @@ for f = 1:length(emgFilenames)
 
 
 
-    %% Get Stims
-
-    
-        
+    %% Plot EMG w/INSOLES
+         
 
     disp('plotting')
     figH = figure; maximize;
     tiledlayout(8,2)
     i = 0;
+
+    mx_cop = max(abs(cop_L));
+    c = [.875 .875 .875];
+
     for m = [9, 1, 10, 2, 11, 3, 12, 4, 13, 5, 14, 6, 15, 7, 16, 8]
         i = i+1;
         ax(i) = nexttile;
         plot(linspace(0,tend,length(data.time)), emg(chan_remap(m),:)*1000);
         hold on; 
-        if stimStatus == 1
-% %             vline([cell2mat(stimEvts)-stimEvts{1}(1)],'r:');
-                scatter([cell2mat(stimEvts)], 0, 0.5, 'r', '+');
-                disp(['Plotting' mLabels(m) '- tile:0' num2str(i)]);     
+
+        if m >=9
+            for g =  2:length(starts_L)
+                x = [t_left(starts_L(g-1)) t_left(stops_L(g)) t_left(stops_L(g)) t_left(starts_L(g-1))]; y = [-mx_cop -mx_cop mx_cop mx_cop];
+                 h1 = fill(x, y, c,'FaceAlpha',0.3, 'EdgeColor','none');
+                if g == length(starts_L)
+                    continue;
+                else
+                     x2 = [t_left(stops_L(g)) t_left(starts_L(g)) t_left(starts_L(g)) t_left(stops_L(g))]; 
+                     h2 = fill(x2, y, 'magenta', 'FaceAlpha',0.05,  'EdgeColor','none');
+                end
+            end
+        elseif m < 9
+            mx_cop = max(abs(cop_R));
+            for g =  2:length(starts_R)
+                x = [t_right(starts_R(g-1)) t_right(stops_R(g)) t_right(stops_R(g)) t_right(starts_R(g-1))]; y = [-mx_cop -mx_cop mx_cop mx_cop];
+                h1 = fill(x, y, c,'FaceAlpha',0.3, 'EdgeColor','none');
+                if g == length(starts_R)
+                    continue;
+                else
+                    x2 = [t_right(stops_R(g)) t_right(starts_R(g)) t_right(starts_R(g)) t_right(stops_R(g))]; 
+                    h2 = fill(x2, y, 'magenta', 'FaceAlpha',0.05,  'EdgeColor','none');
+                end
+            end
         end
-        
+    end
+end
+% %         if stimStatus == 1
+% % % %             vline([cell2mat(stimEvts)-stimEvts{1}(1)],'r:');
+% %                 scatter([cell2mat(stimEvts)], 0, 0.5, 'r', '+');
+% %                 disp(['Plotting' mLabels(m) '- tile:0' num2str(i)]);     
+% %         end
         ylabel([mLabels(chan_remap(m)) ' (mV)']);
         box off
     end
@@ -97,8 +159,7 @@ for f = 1:length(emgFilenames)
     linkaxes([ax(:)], 'x'); xlim([0,tend]); %ylim([-0.5,.5]); 
      
     xlabel ('time (sec)');
-    
-    %stackedplot(outdoors,'Title','Weather Data','DisplayLabels', mLabels)
+   
     
     tit = sgtitle({cell2mat(trialtype), cell2mat(filename)},'interpreter', 'none');
     
